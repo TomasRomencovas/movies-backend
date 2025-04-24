a8d1a300c8c029824f855962b472ee6d310a40fe
Tomas Romencovas <tomas.romencovas@gmail.com>
2025-04-24 22:52:22 +0300

    Fixing cors, take two

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://movies-frontend-beryl-beta.vercel.app/",
+      "https://movies-frontend-beryl-beta.vercel.app",
     ],
     credentials: true,
   })
