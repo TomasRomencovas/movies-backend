564f3434a33f73e5abe73a98752e3809e2bc6041
Tomas Romencovas <tomas.romencovas@gmail.com>
2025-04-24 22:10:57 +0300

    fixing cors for hosting

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, 
+    origin: [
+      "http://localhost:5173",
+      "https://movies-backend-4bx3.onrender.com/",
+    ],
+    credentials: true,
   })
 );
 app.use(express.json());
 
 app.use(router);
 
-app.listen(PORT, () => console.log("Server running on PORT 3000"));
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
